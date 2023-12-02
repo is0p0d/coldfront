6c5b79c1f73589524b2bae1990ede5a45ad23352
is0p0d <jpmoroney715@outlook.com>
2023-12-01 19:04:30 -0600

    initial work on saml plugin

---
+++ coldfront/config/plugins/saml.py
@@ -0,0 +1,22 @@
+from coldfront.config.base import INSTALLED_APPS
+from coldfront.config.env import ENV
+from django.core.exceptions import ImproperlyConfigured
+
+#------------------------------------------------------------------------------
+# Enable saml authentication
+#------------------------------------------------------------------------------
+INSTALLED_APPS += [
+    'django_saml2_auth',
+]
+
+SAML2_AUTH = {    
+    #--------------------------------------------------------------------------
+    # Required Settings (technically only METADATA_AUTO_CONF_URL is needed)
+    #--------------------------------------------------------------------------
+    'METADATA_AUTO_CONF_URL': ENV.str('METADATA_AUTO_CONF_URL')
+    'METADATA_LOCAL_FILE_PATH': ENV.str('METADATA_LOCAL_FILE_PATH')
+    #--------------------------------------------------------------------------
+    # Optional Settings
+    #--------------------------------------------------------------------------
+}
+
---
+++ coldfront/plugins/saml/README.md
@@ -0,0 +1,22 @@
+# SAML2 integration for coldfront
+
+
+
+## environment variables
+
+PLUGIN_AUTH_SAML
+YAML_LOGIN_LINK
+METADATA_AUTO_CONF_URL
+METADATA_LOCAL_FILE_PATH
+
+### files edited:
+
+CREATE: coldfront/plugins/saml/README.md
+CREATE: coldfront/config/plugins/saml.py
+MODIFY: coldfront/config/settings.py
+MODIFY: coldfront/config/urls.py
+MODIFY: coldfront/core/user/templates/user/login.html
+
+### TODO:
+
+finish settings integration in saml.py
